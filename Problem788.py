4fea104529e5e5f04a7ea3bc86237a922f6723bc
Tobi <tobias_kaiser@msn.com>
2022-08-14 18:35:29 +0200

    Solved Project Euler Problem 788

--- Problem788.py
+++ Problem788.py
@@ -1,35 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 14 16:09:06 2022
+
+@author: Tobi
+"""
+
 from sqlalchemy import Boolean
+from scipy.special import binom
 
 
-def check_dominant(n: int) -> Boolean:
-    n_str = str(n)
-    s = set(list(n_str))
-    for i in list(s):
-        if list(n_str).count(i) > len(n_str) / 2:
-            return True
-    return False
-
-N = 6
+mod_const = 10**9+7
+pot_dict = dict({0: 1, 1: 9})
+binom_dict = dict()
+end = 2022
+for i in range(2,2022):
+    pot_dict[i] = 9*pot_dict[i-1] % mod_const
+ 
+for i in range(1,end+1):
+    binom_dict[(i,0)] = 1
+    binom_dict[(i,i)] = 1
+    for j in range(1,i):
+        binom_dict[(i,j)] = (binom_dict[(i-1,j-1)] + binom_dict[(i-1,j)]) % mod_const
+            
 cnt = 0
-# for i in range(10**(N-1),10**N):
-#     if check_dominant(i):
-#         # print(i)
-#         cnt += 1
-
-# print(cnt)
-
-print(check_dominant(232322))
-
-# 1: 9
-# 2: 9
-# 3: 9*9*3 + 9
-# 4: 9*9*4 + 9
-# 5: 9*9*8*(5 over 3) + 9*9*(5 over 3) + 9*9*(5 over 4) + 9
-# 6: 9*9*8*(6 over 4) + 9*9*(6 over 4) + 9*9*(6 over 5) + 9
-# 7: 9*9*8*7*(7 over 4) + 9*9*
-x5 = 603
-x6 = 9*9*8*(5*4) + 9*9*20 + 9*9*6 + 9
-x7 = 9*9*8*7*(7*6*5/6) + 9*9
-
-
-# print(check_dominant(223))
+for i in range(1,end+1):
+    if i%2==0:
+        start_j = int(i/2+1)
+    else:
+        start_j = int(i/2)+1
+    for j in range(start_j, i+1):
+        cnt += 9*pot_dict[(i-j)] % (mod_const) * binom_dict[(i,j)] % (mod_const)
+        cnt %= (mod_const)
+print(cnt) # 471745499
